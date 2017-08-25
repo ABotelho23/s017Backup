484c8e39a7042925764aeaed5710af5fc9e236c6
Alex Botelho <botelho2305@gmail.com>
2017-08-24 22:58:14 -0400

    Create Backup Script.bat

---
+++ Backup Script.bat
@@ -0,0 +1,1 @@
+robocopy "C:\Users" "E:\Staples Backup\Users" /v /log:"E:\robocopylog.txt" /e /zb /mt:4 /r:5 /w:6 /copy:dt /xj /xf "NETUSER.DAT" /xd "AppData" /xd "Application Data" /xd "E:\Users\All Users" /xd "E:\Default User" /xd "E:\Users\Default" /xd "E:\Users\DefaultAppPool" /xd "E:\Users\Default.migrated"
