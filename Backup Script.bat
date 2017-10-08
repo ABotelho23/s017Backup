a3db920b550893ff38040202207b7f557d4b5805
Alex Botelho <botelho2305@gmail.com>
2017-10-08 15:43:23 -0400

    more extra folders stuff
    -also moved invalidletter higher and placed a skip statement above it

--- Backup Script.bat
+++ Backup Script.bat
@@ -17,6 +17,12 @@ SET /P backDestLet=What is the drive letter of the drive to put the backup unto?
 IF /I "%backDestLet%"=="c" GOTO :invalidletter
 IF NOT EXIST "backDestLet%:\" GOTO :invalid letter
 
+GOTO skipinvalidletter
+:invalidletter
+echo This is an invalid selection, try again.
+GOTO :mainmenu
+:skipinvalidletter
+
 mkdir "%backDestLet%:\StaplesBackup"
 mkdir "%backDestLet%:\StaplesBackup\Users"
 robocopy "C:\Users" "%backDestLet%:\StaplesBackup\Users" /v /log:"%backDestLet%:\backupLog.txt" /e /zb /mt:4 /r:3 /w:3 /copy:dt /tee /eta /xj /xf "NETUSER.DAT" /xd "Local Settings" /xd "AppData" /xd "Application Data" /xd "C:\Users\All Users" /xd "C:\Default User" /xd "C:\Users\Default" /xd "C:\Users\DefaultAppPool" /xd "C:\Users\Default.migrated"
@@ -27,7 +33,7 @@ GOTO :end
 :backupextrafolders
 SET extracount=0
 :backupextrainput
-SET /P backupExtraInput=Would you like to backup folders not present under Users[y/n]? 
+SET /P backupExtraInput=Would you like to backup extra folders not present under Users[y/n]? 
 IF "%backupExtraInput%"=="y" GOTO :extrabackuploop
 IF "%backupExtraInput%"=="n" GOTO :end
 echo Invalid selection, please type y or n.
@@ -35,12 +41,20 @@ GOTO :backupextrainput
 
 :extrabackuploop
 SET /A extracount=extracount+1
+SET backupExtraPath[0]="EMPTY"
 mkdir "%backDestLet%:\StaplesBackup\Extra%extracount%"
-SET /P backupExtraPath=What is the full path of the extra folder number %extracount%? 
+SET /P backupExtraPath[extracount]=What is the full path of the extra folder (include drive letter and backslashes, NOT frontslashes) number %extracount%? 
+SET /P backupExtraAgain=Add another folder to extra backups[y/n]? 
+IF "%backupExtraAgain%"=="y" GOTO :extrabackuploop
+IF "%backupExtraAgain%"=="n" GOTO :performextrabackup
+echo Invalid selection, please type y or n.
+GOTO :backupextraloop
 
-:invalidletter
-echo This is an invalid selection, try again.
-GOTO :mainmenu
+:performextrabackup
+SET performcount=0
+:performloop
+
+
 
 :end
 echo TASKS COMPLETE. YOU HAVE REACHED THE END OF THE SCRIPT.
