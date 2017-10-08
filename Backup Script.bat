a6432b6331bc01106165d41db5a05eec98fb0d14
Alex Botelho <botelho2305@gmail.com>
2017-10-08 11:49:48 -0400

    added opening log fie
    -indicates backup is done and opens up the log file for viewing

--- Backup Script.bat
+++ Backup Script.bat
@@ -4,3 +4,5 @@ echo What would you like to do?
 echo 1. Backup data
 echo 2. Migrate data
 robocopy "C:\Users" "E:\Staples Backup\Users" /v /log:"E:\backupLog.txt" /e /zb /mt:4 /r:3 /w:3 /copy:dt /tee /eta /xj /xf "NETUSER.DAT" /xd "Local Settings" /xd "AppData" /xd "Application Data" /xd "E:\Users\All Users" /xd "E:\Default User" /xd "E:\Users\Default" /xd "E:\Users\DefaultAppPool" /xd "E:\Users\Default.migrated"
+echo "BACKUP COMPLETE. Displaying log file.
+start "" "E:\backupLog.txt"
