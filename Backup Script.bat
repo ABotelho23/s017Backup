b98831bf00e00915d1153cf532e3d90070cf620c
Alex Botelho <botelho2305@gmail.com>
2017-10-08 13:21:27 -0400

    Multiple changes to inputs
    -converted "What would you like to do?" to a proper input
    -added mainmenu, and backup jumps/tags
    -added IF for backup/migration, even if migrate not working yet
    -added input for destination drive letter selection
    -added creation of StaplesBackup folder (to ensure it is a unique name later)
    -added selected drive letter in robocopy command

--- Backup Script.bat
+++ Backup Script.bat
@@ -1,8 +1,23 @@
-echo off
-echo Welcome to Staples Tech Backup and Migrate script
-echo What would you like to do?
-echo 1. Backup data
-echo 2. Migrate data
-robocopy "C:\Users" "E:\Staples Backup\Users" /v /log:"E:\backupLog.txt" /e /zb /mt:4 /r:3 /w:3 /copy:dt /tee /eta /xj /xf "NETUSER.DAT" /xd "Local Settings" /xd "AppData" /xd "Application Data" /xd "E:\Users\All Users" /xd "E:\Default User" /xd "E:\Users\Default" /xd "E:\Users\DefaultAppPool" /xd "E:\Users\Default.migrated"
+@echo off
+echo Welcome to Staples Store #17 Tech Backup and Migrate script
+:mainmenu
+echo Main Menu
+echo 1. Backup Data
+echo 2. Migrate Data [COMING SOON]
+
+set /P backMigSel=What would you like to do? 
+
+IF "%backMigSel%"=="1" GOTO :backup
+echo Not a valid selection, please try again.
+GO TO :mainmenu
+
+:backup
+set /P backDestLet=What is the drive letter of the drive to put the backup unto? 
+mkdir "%backDestLet%:\StaplesBackup"
+mkdir %backDestLet%:\StaplesBackup\Users"
+robocopy "C:\Users" "%backDestLet%:\StaplesBackup\Users" /v /log:"%backDestLet%:\backupLog.txt" /e /zb /mt:4 /r:3 /w:3 /copy:dt /tee /eta /xj /xf "NETUSER.DAT" /xd "Local Settings" /xd "AppData" /xd "Application Data" /xd "C:\Users\All Users" /xd "C:\Default User" /xd "C:\Users\Default" /xd "C:\Users\DefaultAppPool" /xd "C:\Users\Default.migrated"
 echo "BACKUP COMPLETE. Displaying log file.
 start "" "E:\backupLog.txt"
+GOTO :end
+
+:end
