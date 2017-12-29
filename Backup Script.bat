9bd9fb88bd632ab9e61927b9c3d3ee58ab10e869
Alex Botelho <botelho2305@gmail.com>
2017-12-29 15:49:17 -0500

    Changed admin timer to 10 seconds to give time to read it

--- Backup Script.bat
+++ Backup Script.bat
@@ -3,8 +3,8 @@ openfiles > NUL 2>&1
 if NOT %ERRORLEVEL% EQU 0 goto :NotAdmin 
 goto :mainmenu 
 :NotAdmin
-echo Please run as admin. Closing in 3 seconds.
-TIMEOUT 3
+echo Please run as admin. Closing in 10 seconds.
+TIMEOUT 10
 GOTO :realend
 
 echo Welcome to Staples Store #17 Tech Backup and Migrate script!
