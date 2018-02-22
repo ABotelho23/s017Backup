351a37b66d3f5496e77969d3996b5236ed08765a
ABotelho23 <botelho2305@gmail.com>
2018-02-21 23:54:34 -0500

    Massive update -Finished up migration before testing -Added support from migrating from old PC -Made more verbose messages to starting backup/migration

--- Backup Script.bat
+++ Backup Script.bat
@@ -15,12 +15,15 @@ echo MAIN MENU - What would you like to do?
 echo 1. Backup Data
 echo 2. Migrate Data [experimental]
 echo 3. Migrate Data (Custom) [COMING SOON]
+echo 4. Migrate Directly from an Old PC [COMING SOON]
 set /P backMigSel=What would you like to do?
 
 IF "%backMigSel%"=="1" GOTO :backup
 IF "%backMigSel%"=="2" GOTO :migration
 IF "%backMigSel%"=="3" GOTO :migrationcustom
-echo That is not a valid selection, please try again.
+IF "%backMigSel%"=="4" GOTO :migrateoldpc
+
+echo That is not a valid selection, returning to main menu.
 GOTO :mainmenu
 
 :backup
@@ -31,13 +34,13 @@ IF NOT EXIST "%backDestLet%:\" GOTO :invalidletterback
 
 echo Valid selection. Starting Backup in 3 seconds.
 TIMEOUT 3
-echo STARTING BACKUP.
+echo STARTING BACKUP from C:\Users to "%backDestLet%:\StaplesBackup\Users".
 
 mkdir "%backDestLet%:\StaplesBackup"
-mkdir %backDestLet%:\StaplesBackup\Backup"
-mkdir %backDestLet%:\StaplesBackup\Backup\StaplesBackup"
-robocopy "C:\Users" "%backDestLet%:\StaplesBackup\Backup\Users" /v /log:"%backDestLet%:\StaplesBackup\backupLog.txt" /e /zb /mt:4 /r:3 /w:3 /copy:dt /tee /eta /xj /xf "NETUSER.DAT" /xd "Local Settings" /xd "AppData" /xd "Application Data" /xd "C:\Users\All Users" /xd "C:\Default User" /xd "C:\Users\Default" /xd "C:\Users\DefaultAppPool" /xd "C:\Users\Default.migrated"
-echo BACKUP COMPLETE. Displaying log file.
+mkdir "%backDestLet%:\StaplesBackup\Backup"
+mkdir "%backDestLet%:\StaplesBackup\Backup\Users"
+robocopy "C:\Users" "%backDestLet%:\StaplesBackup\Backup\Users" /v /log:"%backDestLet%:\StaplesBackup\backupLog.txt" /e /zb /mt:4 /r:3 /w:3 /copy:dt /tee /eta /xj /xf "NETUSER.DAT" /xf "NETUSER.DAT* /xd "Local Settings" /xd "AppData" /xd "Application Data" /xd "C:\Users\All Users" /xd "C:\Default User" /xd "C:\Users\Default" /xd "C:\Users\DefaultAppPool" /xd "C:\Users\Default.migrated"
+echo BACKUP COMPLETE. Please verify. Displaying log file.
 start "" "%backDestLet%:\StaplesBackup\backupLog.txt"
 GOTO :end
 
@@ -49,8 +52,9 @@ IF NOT EXIST "%migSrcLet%:\" GOTO :invalidlettermig
 IF NOT EXIST "%migSrcLet%:\" GOTO :nobackupfound
 
 echo Valid selection. Starting Migration in 3 seconds.
+echo Note: Migration is done to Users folder; if old user folder name is different from new user old, unexpected results may occur.
 TIMEOUT 3
-echo STARTING MIGRATION.
+echo STARTING MIGRATION from "%migSrcLet%:\StaplesBackup\Backup\Users" to "C:\Users".
 
 robocopy "%migSrcLet%:\StaplesBackup\Backup\Users" "C:\Users" /v /log:"C:\Users\migrationLog.txt" /e /zb /mt:4 /r:3 /w:3 /copy:dt /tee /eta
 echo MIGRATION COMPLETE. Displaying log file.
@@ -58,28 +62,51 @@ start "" "C:\Users\migrationLog.txt"
 GOTO :end
 
 :migrationcustom
-echo Custom migrations not yet available. Try again.
+echo Custom migrations not yet available. Please try again.
 GOTO :mainmenu
+
+:migrateoldpc
+set /P backDestLet=What is the drive letter of the old drive connected as?
+IF "%oldSrcLet%"=="c" GOTO :cantusecold
+IF "%oldSrcLet%"=="C" GOTO :cantusecold
+IF NOT EXIST "%oldSrcLet%:\" GOTO :invalidletterold
+
+echo Valid selection. Starting Migration from old PC in 3 seconds.
+TIMEOUT 3
+echo STARTING MIGRATION FROM OLD PC "%oldSrcLet%:\Users" to "C:\Users".
+
+robocopy "%oldSrcLet%:\Users" "C:\Users" /v /log:"C:\Users\migrationLog.txt" /e /zb /mt:4 /r:3 /w:3 /copy:dt /tee /eta /xj /xf "NETUSER.DAT" /xf "NETUSER.DAT*" /xd "Local Settings" /xd "AppData" /xd "Application Data" /xd "%oldSrcLet%:\Users\All Users" /xd "%oldSrcLet%:\Default User" /xd "%oldSrcLet%:\Users\Default" /xd "%oldSrcLet%:\Users\DefaultAppPool" /xd "%oldSrcLet%:\Users\Default.migrated"
+echo MIGRATION FROM OLD PC COMPLETE. Please verify. Displaying log file.
+start "" "C:\Users\migrationLog.txt"
+GOTO :end
 
 :cantusecback
 echo C: drive cannot be used as a destination, please select another drive
+GOTO :backup
+
+:invalidletterback
+echo A drive with that letter is not connected. Please check the letter and try again.
 GOTO :backup
 
 :cantusecmig
 echo C: drive cannot be used as a source, please select another drive
 GOTO :migration
 
-:invalidletterback
-echo A drive with that letter is not connected. Please check the letter and try again.
-GOTO :backup
-
 :invalidlettermig
 echo A drive with that letter is not connected. Please check the letter and try again.
 GOTO :migration
 
 :nobackupfound
-echo Backup not found on the selected drive. Please try again.
+echo Staples-made backup not found on the selected drive. Please try again.
 GOTO :migration
+
+:cantusecold
+echo C: drive cannot be used as a source, please select another drive
+GOTO :migrateoldpc
+
+:invalidletterold
+echo A drive with that letter is not connected. Please check the letter and try again.
+GOTO :migrateoldpc
 
 :end
 pause
