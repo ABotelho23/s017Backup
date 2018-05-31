0412bd51e2f4c2d01c6328245731eda9c0d56701
ABotelho23 <botelho2305@gmail.com>
2018-05-31 17:56:34 -0400

    first Linux/MacOS commit add welcome paragraph and intro

---
+++ s017Backup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+printf '%s'"----------------------------------------------------------------------\n"
+printf "Welcome to Staples Store #17 Tech Backup, Migration and Folder Cloning script!\n"
+printf "See https://github.com/ABotelho23/s017Backup for more information\n"
+printf "Created by Alex Botelho with the help of Aaron Langlois and Thomas Belway\n"
+printf '%s'"----------------------------------------------------------------------\n"
