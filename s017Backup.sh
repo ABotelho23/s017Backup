263e210f0a56ac52a7fb864ab64757b283839dff
ABotelho23 <botelho2305@gmail.com>
2018-08-07 14:51:35 -0400

    cleaned up menu, added man menu title

--- s017Backup.sh
+++ s017Backup.sh
@@ -5,21 +5,26 @@ printf "See https://github.com/ABotelho23/s017Backup for more information\n"
 printf "Created by Alex Botelho with the help of Aaron Langlois and Thomas Belway\n"
 printf '%s'"----------------------------------------------------------------------\n"
 
-PS3='MAIN MENU'
-options=("1. Backup Users Folder Only (Windows)" "2. Backup Users Folder Only (MacOS/OSX)" "3. Backup Entire Drive" "4. Backup Specific Folder" "Quit")
+printf "\n=== MAIN MENU ===\n"
+
+PS3='What would you like to do?'
+options=("Backup Users Folder Only (Windows)" "Backup Users Folder Only (MacOS/OSX)" "Backup Users Folder Only (Linux/GNU)" "Backup Entire Drive" "Backup Specific Folder" "Quit")
 select opt in "${options[@]}"
 do
   case $opt in
     "Backup Users Folder Only (Windows)")
       printf "Starting Backup of Windows Users folder..."
       ;;
-    "2. Backup Users Folder Only (MacOS/OSX)")
+    "Backup Users Folder Only (MacOS/OSX)")
       printf "Starting Backup of MacOS Users folder..."
       ;;
-    "3. Backup Entire Drive")
+    "Backup Users Folder Only (Linux/GNU)")
+      printf "Starting Backup of Linux/GNU home folder..."
+      ;;
+    "Backup Entire Drive")
       printf "Starting Backup of Selected Drive..."
       ;;
-    "4. Backup Specific Folder")
+    "Backup Specific Folder")
       printf "Start Backup of Specified Drive..."
       ;;
     "Quit")
