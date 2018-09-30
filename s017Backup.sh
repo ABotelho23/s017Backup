2613a84c101fa2ece76ca9b8174aa3ca32c20dd8
ABotelho23 <botelho2305@gmail.com>
2018-09-30 14:25:27 -0400

    MacOS Backup working

--- s017Backup.sh
+++ s017Backup.sh
@@ -3,11 +3,11 @@
 #check OS
 if [ `uname` = "Linux" ]
 then
-  os_check=Linux
+  os_check="Linux"
 
 elif [ `uname` = "Darwin" ]
 then
-  os_check=MacOS
+  os_check="MacOS"
 
 else
   printf "Sorry, your OS is not supported. Exiting in 5 seconds."
@@ -24,31 +24,57 @@ printf '%s'"--------------------------------------------------------------------
 printf "\n========== MAIN MENU ==========\n"
 printf "OS: $os_check detected.\n"
 PS3='What would you like to do? '
-options=("Backup Users Folder Only (Windows)" "Backup Users Folder Only (MacOS/OSX or GNU/Linux)" "Backup Entire Drive" "Backup Specific Folder" "Quit")
+options=("Backup Users/Home Folder Only" "Migrate from an existing backup" "Backup Entire Drive" "Backup Specific Folder" "Quit")
 select opt in "${options[@]}"
 do
   case $opt in
-    "Backup Users Folder Only (Windows)")
-      printf "Starting Backup of Windows Users folder..."
-      mkdir "/media/StaplesBackup"
-      rsync ...
-      ;;
-    "Backup Users Folder Only (MacOS/OSX or GNU/Linux)")
+    "Backup Users/Home Folder Only")
+      echo "Backup selected."
       if [ $os_check = "MacOS" ]
       then
+        clear
+        while [ "$dirExists" != "true" ]; do
+          printf "Available drives:"
+          ls /Volumes
+          read -p 'Which volume from the list is the drive to place Backup unto? (Names are case-sensitive!) ' backupDes
+          if [ -d "/Volumes/$backupDes" ]; then
+          dirExists="true"
+          fi
+        done
         printf "Starting Backup of MacOS Users folder..."
-        #ask which Volume it should be backed up to
-        rsync --verbose --recursive --human-readable --progress --log-file=""/Volumes/BACKUPDRIVE/StaplesBackup/backupLog.txt" --exclude=.cache --exclude=.Trash --exclude=.plist --exclude=Library --exclude=.DS_Store --exclude=Sites --exclude=AirPort.networkConnect --exclude=.CFUserTextEncoding  --exclude=.cups /Users/ /Volumes/BACKUPDRIVE/StaplesBackup/Backup/Users
+        mkdir /Volumes/$backupDes
+        mkdir /Volumes/$backupDes/StaplesBackup
+        mkdir /Volumes/$backupDes/StaplesBackup/Backup
+        rsync --verbose --recursive --human-readable --progress --log-file="/Volumes/$backupDes/StaplesBackup/backupLog.txt" --exclude=.cache --exclude=.Trash --exclude=.plist --exclude=Library --exclude=.DS_Store --exclude=Sites --exclude=AirPort.networkConnect --exclude=.CFUserTextEncoding  --exclude=.cups "/Users/" "/Volumes/$backupDes/StaplesBackup/Backup/Users"
       elif [ $os_check = "Linux" ]
       then
-        printf "Starting Backup of GNU/Linux home folder"
-        rsync ... Linux
+        printf "ERROR: LINUX BACKUP NOT AVAILABLE YET."
+        #rsync ... Linux
       else
         printf "Unsure how OS detection was bypassed. Exiting in 5 seconds."
         sleep 5
         exit 1
       fi
+      break
       ;;
+
+    "Migrate from an existing backup")
+      if [ $os_check = "MacOS" ]
+      then
+        printf "Not available yet."
+        #INPUT NAME OF DRIVE TO MIGRATE
+      elif [ $os_check = "Linux" ]
+      then
+        printf "Not available yet."
+        #INPUT NAME OF DRIVE TO MIGRATE
+      else
+        printf "Unsure how OS detection was bypassed. Exiting in 5 seconds."
+        sleep 5
+        exit 1
+      fi
+      break
+      ;;
+
     "Backup Entire Partition")
       if [ $os_check = "MacOS" ]
       then
@@ -63,6 +89,7 @@ do
         sleep 5
         exit 1
       fi
+      break
       ;;
     "Backup Specific Folder")
       read -p 'What is the full path of the folder to backup? ' inputbackupfolder
